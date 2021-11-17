88b82928c4ad3be3a777e9d0d766f63abb1779f5
Opilane TTHK <opilane@tthk.ee>
2021-11-17 12:54:44 +0200

    week12

---
+++ CreateTitpe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CreateTitpe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            string directory = "Titpe21";
+            string createDirectory = $@"{rootPath}\{directory}";
+
+            bool directoryExists = Directory.Exists(createDirectory);
+
+            if (directoryExists)
+            {
+                Console.WriteLine("See kaust on juba olemas");
+            }
+            else
+            {
+                Console.WriteLine($"Kaust {directory} on loodud");
+                Directory.CreateDirectory(createDirectory);
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //preapare a list to save data from file
+            List<string> toDoFromFile = new List<string>();
+            string toDoFilePath = @"";
+            toDoFromFile = File.ReadAllLines(toDoFilesPath).ToList();
+
+
+            foreach(string task in toDoFromList)
+            {
+                Console.WriteLine(task);
+            }
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a task Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if(userInput == 'y')
+                {
+                    Console.WriteLine("Enter a task");
+                    string userTAsk = Console.Readlne();
+                    toDoFromList.Add(userTask);
+                    Console.WriteLine($"Task {userTask} added.");
+                }
+                else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+            FileStyleUriParser.WriteAllLines(toDoFilePath, ToDoFromFile.ToArray());
+
+
+
+        }
+    }
+}
